3c29d28d06f21e6cd47853bde61080027e29ec17
Carlos Calvo <carloscalvonazabal@gmail.com>
2021-03-02 22:54:57 -0300

    navbar added

---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+const NavBar = () => (
+  <Navbar id='navbar' expand="sm" variant="dark">
+    <Nav.Link id="inicio" href="/home">Inicio</Nav.Link>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <NavDropdown title="Productos" id="basic-nav-dropdown">
+          <NavDropdown.Item href="/productos">Todos los productos</NavDropdown.Item>
+          <NavDropdown.Item href="/nenas">Nenas</NavDropdown.Item>
+          <NavDropdown.Item href="/pre-teens">Pre Teens</NavDropdown.Item>
+          <NavDropdown.Item href="/chiquititas">Chiquititas</NavDropdown.Item>
+          <NavDropdown.Item href="/varones">Varones</NavDropdown.Item>
+          <NavDropdown.Item href="/sale">Sale</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="/sale">Sale</Nav.Link>
+        <Nav.Link href="/ayuda">Cómo comprar</Nav.Link>
+        <Nav.Link href="/tabla-de-talles">Tabla de talles</Nav.Link>
+        <Nav.Link href="/preguntas-frecuentes">Preguntas Frecuentes</Nav.Link>
+        <Nav.Link href="/ventas-mayoristas">Ventas Mayoristas</Nav.Link>        
+        <Nav.Link href="/quienes-somos">Quiénes somos</Nav.Link>
+        <Nav.Link href="/contacto">Contacto</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" size="sm" placeholder="Buscar..." className="mr-sm-2" />
+        <Button size="sm" variant="light">Buscar</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+)
+
+export default NavBar
+
+
+/*
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div id="main">
+        <img id="logo" src={logo} alt="logo"/>
+      </div>
+    </>
+  );
+}
+*/
